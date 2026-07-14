f3fa4c3a0180270d1ae8e7e386454952ad16aa87
janani <jananijanani2611@gmail.com>
2026-07-14 15:59:40 +0530

    File updated at 14/07/2026, 15:59:39

--- 57-InsertInterval/57-InsertInterval.java
+++ 57-InsertInterval/57-InsertInterval.java
@@ -1,35 +1,42 @@
-// Last updated: 14/07/2026, 15:59:08
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public int[][] insert(int[][] intervals, int[] newInterval) {
-6        List<int[]> result = new ArrayList<>();
-7        int i = 0;
-8        int n = intervals.length;
+// Last updated: 14/07/2026, 15:59:39
+1class Solution {
+2    public int[][] generateMatrix(int n) {
+3        int[][] matrix = new int[n][n];
+4        
+5        int top = 0;
+6        int bottom = n - 1;
+7        int left = 0;
+8        int right = n - 1;
 9        
-10        // 1. Add all intervals that come before the new interval
-11        while (i < n && intervals[i][1] < newInterval[0]) {
-12            result.add(intervals[i]);
-13            i++;
-14        }
-15        
-16        // 2. Merge all overlapping intervals with the new interval
-17        while (i < n && intervals[i][0] <= newInterval[1]) {
-18            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-19            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-20            i++;
-21        }
-22        // Add the merged interval
-23        result.add(newInterval);
-24        
-25        // 3. Add all remaining intervals
-26        while (i < n) {
-27            result.add(intervals[i]);
-28            i++;
-29        }
-30        
-31        // Convert the List to a 2D array
-32        return result.toArray(new int[result.size()][]);
-33    }
-34}
+10        int num = 1;
+11        int maxNum = n * n;
+12        
+13        while (num <= maxNum) {
+14            // 1. Traverse from left to right along the top row
+15            for (int j = left; j <= right; j++) {
+16                matrix[top][j] = num++;
+17            }
+18            top++;
+19            
+20            // 2. Traverse from top to bottom along the right column
+21            for (int i = top; i <= bottom; i++) {
+22                matrix[i][right] = num++;
+23            }
+24            right--;
+25            
+26            // 3. Traverse from right to left along the bottom row
+27            for (int j = right; j >= left; j--) {
+28                matrix[bottom][j] = num++;
+29            }
+30            bottom--;
+31            
+32            // 4. Traverse from bottom to top along the left column
+33            for (int i = bottom; i >= top; i--) {
+34                matrix[i][left] = num++;
+35            }
+36            left++;
+37        }
+38        
+39        return matrix;
+40    }
+41}
