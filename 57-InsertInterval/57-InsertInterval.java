51ae194acf8e0182ec6e392c9016f4fa15713f7a
janani <jananijanani2611@gmail.com>
2026-07-14 16:02:02 +0530

    File updated at 14/07/2026, 16:02:01

--- 57-InsertInterval/57-InsertInterval.java
+++ 57-InsertInterval/57-InsertInterval.java
@@ -1,42 +1,50 @@
-// Last updated: 14/07/2026, 15:59:39
-1class Solution {
-2    public int[][] generateMatrix(int n) {
-3        int[][] matrix = new int[n][n];
-4        
-5        int top = 0;
-6        int bottom = n - 1;
-7        int left = 0;
-8        int right = n - 1;
-9        
-10        int num = 1;
-11        int maxNum = n * n;
-12        
-13        while (num <= maxNum) {
-14            // 1. Traverse from left to right along the top row
-15            for (int j = left; j <= right; j++) {
-16                matrix[top][j] = num++;
-17            }
-18            top++;
-19            
-20            // 2. Traverse from top to bottom along the right column
-21            for (int i = top; i <= bottom; i++) {
-22                matrix[i][right] = num++;
-23            }
-24            right--;
-25            
-26            // 3. Traverse from right to left along the bottom row
-27            for (int j = right; j >= left; j--) {
-28                matrix[bottom][j] = num++;
-29            }
-30            bottom--;
-31            
-32            // 4. Traverse from bottom to top along the left column
-33            for (int i = bottom; i >= top; i--) {
-34                matrix[i][left] = num++;
-35            }
-36            left++;
-37        }
-38        
-39        return matrix;
-40    }
-41}
+// Last updated: 14/07/2026, 16:02:01
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode rotateRight(ListNode head, int k) {
+13        // Base cases: empty list, single node, or no rotation needed
+14        if (head == null || head.next == null || k == 0) {
+15            return head;
+16        }
+17        
+18        // 1. Calculate the length of the list and find the tail node
+19        ListNode tail = head;
+20        int length = 1;
+21        while (tail.next != null) {
+22            tail = tail.next;
+23            length++;
+24        }
+25        
+26        // 2. Adjust k to be within the bounds of the list size
+27        k = k % length;
+28        if (k == 0) {
+29            return head; // No rotation needed
+30        }
+31        
+32        // 3. Connect the tail to the head to form a circular loop
+33        tail.next = head;
+34        
+35        // 4. Find the new tail node
+36        // The new tail is at index (length - k - 1) from the start
+37        int stepsToNewTail = length - k;
+38        ListNode newTail = tail; 
+39        for (int i = 0; i < stepsToNewTail; i++) {
+40            newTail = newTail.next;
+41        }
+42        
+43        // 5. Break the circle and update the head
+44        ListNode newHead = newTail.next;
+45        newTail.next = null;
+46        
+47        return newHead;
+48    }
+49}
