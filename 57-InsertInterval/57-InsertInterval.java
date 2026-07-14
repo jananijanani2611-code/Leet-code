19d11254879609ecd0731ab5695ea019f581afd4
janani <jananijanani2611@gmail.com>
2026-07-14 16:02:30 +0530

    File updated at 14/07/2026, 16:02:30

--- 57-InsertInterval/57-InsertInterval.java
+++ 57-InsertInterval/57-InsertInterval.java
@@ -1,50 +1,22 @@
-// Last updated: 14/07/2026, 16:02:01
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode rotateRight(ListNode head, int k) {
-13        // Base cases: empty list, single node, or no rotation needed
-14        if (head == null || head.next == null || k == 0) {
-15            return head;
-16        }
-17        
-18        // 1. Calculate the length of the list and find the tail node
-19        ListNode tail = head;
-20        int length = 1;
-21        while (tail.next != null) {
-22            tail = tail.next;
-23            length++;
-24        }
-25        
-26        // 2. Adjust k to be within the bounds of the list size
-27        k = k % length;
-28        if (k == 0) {
-29            return head; // No rotation needed
-30        }
-31        
-32        // 3. Connect the tail to the head to form a circular loop
-33        tail.next = head;
-34        
-35        // 4. Find the new tail node
-36        // The new tail is at index (length - k - 1) from the start
-37        int stepsToNewTail = length - k;
-38        ListNode newTail = tail; 
-39        for (int i = 0; i < stepsToNewTail; i++) {
-40            newTail = newTail.next;
-41        }
-42        
-43        // 5. Break the circle and update the head
-44        ListNode newHead = newTail.next;
-45        newTail.next = null;
-46        
-47        return newHead;
-48    }
-49}
+// Last updated: 14/07/2026, 16:02:30
+1class Solution {
+2    public int uniquePaths(int m, int n) {
+3        // Optimize space: use a 1D array of size n
+4        int[] dp = new int[n];
+5        
+6        // Base case: There is exactly 1 way to reach any cell in the first row
+7        for (int j = 0; j < n; j++) {
+8            dp[j] = 1;
+9        }
+10        
+11        // Build the DP table row by row
+12        for (int i = 1; i < m; i++) {
+13            for (int j = 1; j < n; j++) {
+14                // dp[j] (new) = dp[j] (from above row) + dp[j-1] (from left neighbor)
+15                dp[j] += dp[j - 1];
+16            }
+17        }
+18        
+19        return dp[n - 1];
+20    }
+21}
