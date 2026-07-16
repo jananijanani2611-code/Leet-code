60418e3cf742c57f435e50786ab709a832282415
janani <jananijanani2611@gmail.com>
2026-07-16 09:03:14 +0530

    File updated at 16/07/2026, 09:03:13

--- 140-WordBreakII/140-WordBreakII.java
+++ 140-WordBreakII/140-WordBreakII.java
@@ -1,71 +1,48 @@
-// Last updated: 16/07/2026, 09:02:32
-1import java.util.*;
-2
-3class Solution {
-4    // Standard Trie Node class
-5    class TrieNode {
-6        TrieNode[] children = new TrieNode[26];
-7        String word = null; // Stores the complete word at the leaf node
-8    }
-9
-10    public List<String> findWords(char[][] board, String[] words) {
-11        List<String> result = new ArrayList<>();
-12        TrieNode root = buildTrie(words);
-13
-14        int m = board.length;
-15        int n = board[0].length;
-16
-17        // Traverse every cell in the matrix as a starting point
-18        for (int i = 0; i < m; i++) {
-19            for (int j = 0; j < n; j++) {
-20                dfs(board, i, j, root, result);
-21            }
-22        }
-23
-24        return result;
-25    }
-26
-27    private void dfs(char[][] board, int r, int c, TrieNode node, List<String> result) {
-28        char ch = board[r][c];
-29        
-30        // Base case: if cell is already visited ('#') or no prefix matches in Trie
-31        if (ch == '#' || node.children[ch - 'a'] == null) {
-32            return;
-33        }
-34
-35        node = node.children[ch - 'a'];
-36        
-37        // If a word is found, add it to the result and clear it to prevent duplicates
-38        if (node.word != null) {
-39            result.add(node.word);
-40            node.word = null; 
-41        }
-42
-43        // Mark the current cell as visited
-44        board[r][c] = '#';
-45
-46        // Explore all 4 neighbors (Up, Down, Left, Right)
-47        if (r > 0) dfs(board, r - 1, c, node, result);
-48        if (r < board.length - 1) dfs(board, r + 1, c, node, result);
-49        if (c > 0) dfs(board, r, c - 1, node, result);
-50        if (c < board[0].length - 1) dfs(board, r, c + 1, node, result);
-51
-52        board[r][c] = ch;
-53    }
-54
-55    private TrieNode buildTrie(String[] words) {
-56        TrieNode root = new TrieNode();
-57        for (String w : words) {
-58            TrieNode node = root;
-59            for (char c : w.toCharArray()) {
-60                int idx = c - 'a';
-61                if (node.children[idx] == null) {
-62                    node.children[idx] = new TrieNode();
-63                }
-64                node = node.children[idx];
-65            }
-66            node.word = w; 
-67        }
-68        return root;
-69    }
-70}
+// Last updated: 16/07/2026, 09:03:13
+1class Solution {
+2    public String shortestPalindrome(String s) {
+3        if (s == null || s.length() <= 1) {
+4            return s;
+5        }
+6
+7        // Reverse the original string
+8        String reversed = new StringBuilder(s).reverse().toString();
+9        
+10        // Combine s and reversed with a separator
+11        String temp = s + "#" + reversed;
+12        
+13        // Compute the KMP LPS array for the combined string
+14        int[] lps = computeLPS(temp);
+15        
+16        // The length of the longest palindromic prefix
+17        int longestPalindromicPrefixLength = lps[temp.length() - 1];
+18        
+19        // The remaining part of the reversed string needs to be added to the front
+20        String suffixToAdd = reversed.substring(0, s.length() - longestPalindromicPrefixLength);
+21        
+22        return suffixToAdd + s;
+23    }
+24
+25    private int[] computeLPS(String str) {
+26        int n = str.length();
+27        int[] lps = new int[n];
+28        int len = 0; // Length of the previous longest prefix suffix
+29        int i = 1;
+30
+31        while (i < n) {
+32            if (str.charAt(i) == str.charAt(len)) {
+33                len++;
+34                lps[i] = len;
+35                i++;
+36            } else {
+37                if (len != 0) {
+38                    len = lps[len - 1]; // Fallback to the previous match
+39                } else {
+40                    lps[i] = 0;
+41                    i++;
+42                }
+43            }
+44        }
+45        return lps;
+46    }
+47}
