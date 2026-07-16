fe45888cd6072121ba3ca2a9da3ab03894900f35
janani <jananijanani2611@gmail.com>
2026-07-16 09:01:28 +0530

    File updated at 16/07/2026, 09:01:28

--- 140-WordBreakII/140-WordBreakII.java
+++ 140-WordBreakII/140-WordBreakII.java
@@ -1,46 +1,38 @@
-// Last updated: 16/07/2026, 08:59:31
-1import java.util.*;
+// Last updated: 16/07/2026, 09:01:28
+1import java.util.Arrays;
 2
 3class Solution {
-4    
-5    private Map<Integer, List<String>> memo;
-6    private Set<String> wordSet;
-7
-8    public List<String> wordBreak(String s, List<String> wordDict) {
-9        this.memo = new HashMap<>();
-10        this.wordSet = new HashSet<>(wordDict); // O(1) lookups
-11        return backtrack(s, 0);
-12    }
-13
-14    private List<String> backtrack(String s, int start) {
-15        
-16        if (memo.containsKey(start)) {
-17            return memo.get(start);
-18        }
-19
-20        List<String> results = new ArrayList<>();
-21        if (start == s.length()) {
-22            results.add("");
-23            return results;
-24        }
+4    public int maxProfit(int k, int[] prices) {
+5        int n = prices.length;
+6        if (n <= 1 || k == 0) {
+7            return 0;
+8        }
+9        if (k >= n / 2) {
+10            int maxProfit = 0;
+11            for (int i = 1; i < n; i++) {
+12                if (prices[i] > prices[i - 1]) {
+13                    maxProfit += prices[i] - prices[i - 1];
+14                }
+15            }
+16            return maxProfit;
+17        }
+18
+19     
+20        int[] buy = new int[k + 1];
+21        int[] sell = new int[k + 1];
+22
+23     
+24        Arrays.fill(buy, Integer.MIN_VALUE);
 25
-26        for (int end = start + 1; end <= s.length(); end++) {
-27            String prefix = s.substring(start, end);
-28            
-29          
-30            if (wordSet.contains(prefix)) {
-31                List<String> subSentences = backtrack(s, end);
-32                
-33                for (String sub : subSentences) {
-34                    if (sub.isEmpty()) {
-35                        results.add(prefix); 
-36                    } else {
-37                        results.add(prefix + " " + sub);
-38                    }
-39                }
-40            }
-41        }
-42        memo.put(start, results);
-43        return results;
-44    }
-45}
+26        for (int price : prices) {
+27            for (int i = 1; i <= k; i++) {
+28               
+29                buy[i] = Math.max(buy[i], sell[i - 1] - price);
+30              
+31                sell[i] = Math.max(sell[i], buy[i] + price);
+32            }
+33        }
+34
+35        return sell[k];
+36    }
+37}
