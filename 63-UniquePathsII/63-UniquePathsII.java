cb924de102016a74210faeb3b7fb5aa8e2f8a3f9
janani <jananijanani2611@gmail.com>
2026-07-19 20:06:51 +0530

    File updated at 19/07/2026, 20:06:49

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,27 +1,23 @@
-// Last updated: 19/07/2026, 20:06:06
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public List<List<Integer>> subsets(int[] nums) {
-6        List<List<Integer>> result = new ArrayList<>();
-7        backtrack(0, nums, new ArrayList<>(), result);
-8        return result;
-9    }
-10
-11    private void backtrack(int start, int[] nums, List<Integer> path, List<List<Integer>> result) {
-12        // Every state of path is a valid subset, add it to the result
-13        result.add(new ArrayList<>(path));
-14
-15        for (int i = start; i < nums.length; i++) {
-16            // Include the current element
-17            path.add(nums[i]);
-18
-19            // Move to the next element to build upon the current subset
-20            backtrack(i + 1, nums, path, result);
-21
-22            // Backtrack: exclude the current element before the next iteration
-23            path.remove(path.size() - 1);
-24        }
-25    }
-26}
+// Last updated: 19/07/2026, 20:06:49
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        // If the array has 2 or fewer elements, it already satisfies the condition
+4        if (nums.length <= 2) {
+5            return nums.length;
+6        }
+7        
+8        // Pointer to place the next valid element
+9        int k = 2;
+10        
+11        // Iterate through the array starting from the 3rd element
+12        for (int i = 2; i < nums.length; i++) {
+13            // Check if the current element is different from the element placed 2 positions back
+14            if (nums[i] != nums[k - 2]) {
+15                nums[k] = nums[i];
+16                k++;
+17            }
+18        }
+19        
+20        return k;
+21    }
+22}
