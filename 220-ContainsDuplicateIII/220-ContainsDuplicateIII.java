8104845747451dfd5adaf797331f1aeb2dbc8396
janani <jananijanani2611@gmail.com>
2026-07-19 20:32:07 +0530

    File updated at 19/07/2026, 20:32:05

--- 220-ContainsDuplicateIII/220-ContainsDuplicateIII.java
+++ 220-ContainsDuplicateIII/220-ContainsDuplicateIII.java
@@ -1,29 +1,52 @@
-// Last updated: 19/07/2026, 20:27:26
-1class Solution {
-2    public boolean isSelfCrossing(int[] distance) {
-3        int n = distance.length;
-4        if (n <= 3) return false;
-5        
-6        for (int i = 3; i < n; i++) {
-7     
-8            if (distance[i] >= distance[i - 2] && distance[i - 1] <= distance[i - 3]) {
-9                return true;
-10            }
-11            
-12 
-13            if (i >= 4 && distance[i - 1] == distance[i - 3] && distance[i] + distance[i - 4] >= distance[i - 2]) {
-14                return true;
-15            }
+// Last updated: 19/07/2026, 20:32:05
+1import java.util.*;
+2
+3class Solution {
+4    public List<List<Integer>> palindromePairs(String[] words) {
+5        List<List<Integer>> pairs = new ArrayList<>();
+6        if (words == null || words.length < 2) return pairs;
+7        
+8        Map<String, Integer> map = new HashMap<>();
+9        for (int i = 0; i < words.length; i++) {
+10            map.put(words[i], i);
+11        }
+12        
+13        for (int i = 0; i < words.length; i++) {
+14            String word = words[i];
+15            int len = word.length();
 16            
-17      
-18            if (i >= 5 && distance[i - 1] <= distance[i - 3] 
-19                       && distance[i - 1] + distance[i - 5] >= distance[i - 3] 
-20                       && distance[i - 2] >= distance[i - 4] 
-21                       && distance[i] + distance[i - 4] >= distance[i - 2]) {
-22                return true;
-23            }
-24        }
-25        
-26        return false;
-27    }
-28}
+17            for (int j = 0; j <= len; j++) {
+18                String left = word.substring(0, j);
+19                String right = word.substring(j);
+20                
+21                // Case 1: left is a palindrome, check if reversed(right) exists
+22                if (isPalindrome(left)) {
+23                    String revRight = new StringBuilder(right).reverse().toString();
+24                    if (map.containsKey(revRight) && map.get(revRight) != i) {
+25                        pairs.add(Arrays.asList(map.get(revRight), i));
+26                    }
+27                }
+28                
+29                // Case 2: right is a palindrome, check if reversed(left) exists
+30                // If right is empty (j == len), it is already fully handled as an empty left in Case 1 
+31                // for the other word, so we only run this when j < len.
+32                if (j < len && isPalindrome(right)) {
+33                    String revLeft = new StringBuilder(left).reverse().toString();
+34                    if (map.containsKey(revLeft) && map.get(revLeft) != i) {
+35                        pairs.add(Arrays.asList(i, map.get(revLeft)));
+36                    }
+37                }
+38            }
+39        }
+40        
+41        return pairs;
+42    }
+43    
+44    private boolean isPalindrome(String s) {
+45        int left = 0, right = s.length() - 1;
+46        while (left < right) {
+47            if (s.charAt(left++) != s.charAt(right--)) return false;
+48        }
+49        return true;
+50    }
+51}
