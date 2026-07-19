eec96576450620dfbfcb8bbe1fec46858edda36f
janani <jananijanani2611@gmail.com>
2026-07-19 19:51:37 +0530

    File updated at 19/07/2026, 19:51:35

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,27 +1,30 @@
-// Last updated: 19/07/2026, 19:50:56
-1class Solution {
-2    public int minPathSum(int[][] grid) {
-3        int m = grid.length;
-4        int n = grid[0].length;
-5        
-6        // Fill the first row (can only come from the left)
-7        for (int j = 1; j < n; j++) {
-8            grid[0][j] += grid[0][j - 1];
-9        }
-10        
-11        // Fill the first column (can only come from above)
-12        for (int i = 1; i < m; i++) {
-13            grid[i][0] += grid[i - 1][0];
-14        }
-15        
-16        // Fill the rest of the grid
-17        for (int i = 1; i < m; i++) {
-18            for (int j = 1; j < n; j++) {
-19                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
-20            }
-21        }
-22        
-23        // The bottom-right cell now contains the minimum path sum
-24        return grid[m - 1][n - 1];
-25    }
-26}
+// Last updated: 19/07/2026, 19:51:35
+1import java.util.Deque;
+2import java.util.ArrayDeque;
+3
+4class Solution {
+5    public String simplifyPath(String path) {
+6        Deque<String> stack = new ArrayDeque<>();
+7        // Split the path by one or more slashes
+8        String[] tokens = path.split("/");
+9        
+10        for (String token : tokens) {
+11            if (token.equals("..")) {
+12                if (!stack.isEmpty()) {
+13                    stack.pollLast(); // Move up to the parent directory
+14                }
+15            } else if (!token.equals(".") && !token.isEmpty()) {
+16                stack.offerLast(token); // Push valid directory name
+17            }
+18        }
+19        
+20        // Reconstruct the canonical path
+21        StringBuilder result = new StringBuilder();
+22        for (String dir : stack) {
+23            result.append("/").append(dir);
+24        }
+25        
+26        // If the stack was empty, return the root directory "/"
+27        return result.length() == 0 ? "/" : result.toString();
+28    }
+29}
