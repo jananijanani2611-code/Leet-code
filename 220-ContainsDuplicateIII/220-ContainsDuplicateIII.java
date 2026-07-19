5941f25f88f46cae59a3a76bc47812d5c3756a23
janani <jananijanani2611@gmail.com>
2026-07-19 20:33:07 +0530

    File updated at 19/07/2026, 20:33:05

--- 220-ContainsDuplicateIII/220-ContainsDuplicateIII.java
+++ 220-ContainsDuplicateIII/220-ContainsDuplicateIII.java
@@ -1,52 +1,50 @@
-// Last updated: 19/07/2026, 20:32:05
+// Last updated: 19/07/2026, 20:33:05
 1import java.util.*;
 2
-3class Solution {
-4    public List<List<Integer>> palindromePairs(String[] words) {
-5        List<List<Integer>> pairs = new ArrayList<>();
-6        if (words == null || words.length < 2) return pairs;
-7        
-8        Map<String, Integer> map = new HashMap<>();
-9        for (int i = 0; i < words.length; i++) {
-10            map.put(words[i], i);
-11        }
+3class SummaryRanges {
+4    private TreeMap<Integer, int[]> treeMap;
+5
+6    public SummaryRanges() {
+7        treeMap = new TreeMap<>();
+8    }
+9    
+10    public void addNum(int value) {
+11        if (treeMap.containsKey(value)) return;
 12        
-13        for (int i = 0; i < words.length; i++) {
-14            String word = words[i];
-15            int len = word.length();
-16            
-17            for (int j = 0; j <= len; j++) {
-18                String left = word.substring(0, j);
-19                String right = word.substring(j);
-20                
-21                // Case 1: left is a palindrome, check if reversed(right) exists
-22                if (isPalindrome(left)) {
-23                    String revRight = new StringBuilder(right).reverse().toString();
-24                    if (map.containsKey(revRight) && map.get(revRight) != i) {
-25                        pairs.add(Arrays.asList(map.get(revRight), i));
-26                    }
-27                }
-28                
-29                // Case 2: right is a palindrome, check if reversed(left) exists
-30                // If right is empty (j == len), it is already fully handled as an empty left in Case 1 
-31                // for the other word, so we only run this when j < len.
-32                if (j < len && isPalindrome(right)) {
-33                    String revLeft = new StringBuilder(left).reverse().toString();
-34                    if (map.containsKey(revLeft) && map.get(revLeft) != i) {
-35                        pairs.add(Arrays.asList(i, map.get(revLeft)));
-36                    }
-37                }
-38            }
-39        }
-40        
-41        return pairs;
-42    }
-43    
-44    private boolean isPalindrome(String s) {
-45        int left = 0, right = s.length() - 1;
-46        while (left < right) {
-47            if (s.charAt(left++) != s.charAt(right--)) return false;
-48        }
-49        return true;
-50    }
-51}
+13        Integer l = treeMap.lowerKey(value);
+14        Integer h = treeMap.higherKey(value);
+15        
+16        // Case 1: value is already covered by the lower interval
+17        if (l != null && treeMap.get(l)[1] >= value) {
+18            return;
+19        }
+20        
+21        boolean mergeWithLeft = (l != null && treeMap.get(l)[1] + 1 == value);
+22        boolean mergeWithRight = (h != null && h == value + 1);
+23        
+24        if (mergeWithLeft && mergeWithRight) {
+25            // Case 2: value bridges both lower and higher intervals
+26            treeMap.get(l)[1] = treeMap.get(h)[1];
+27            treeMap.remove(h);
+28        } else if (mergeWithLeft) {
+29            // Case 3: value extends the lower interval to the right
+30            treeMap.get(l)[1] = value;
+31        } else if (mergeWithRight) {
+32            // Case 4: value extends the higher interval to the left
+33            int[] rightInterval = treeMap.remove(h);
+34            treeMap.put(value, new int[]{value, rightInterval[1]});
+35        } else {
+36            // Case 5: value creates a standalone single-element interval
+37            treeMap.put(value, new int[]{value, value});
+38        }
+39    }
+40    
+41    public int[][] getIntervals() {
+42        int[][] result = new int[treeMap.size()][2];
+43        int i = 0;
+44        for (int[] interval : treeMap.values()) {
+45            result[i++] = interval;
+46        }
+47        return result;
+48    }
+49}
