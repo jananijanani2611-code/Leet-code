24f7e4405054bb581cec87611657fa4bdd71e982
janani <jananijanani2611@gmail.com>
2026-07-19 20:12:36 +0530

    File updated at 19/07/2026, 20:12:34

--- 220-ContainsDuplicateIII/220-ContainsDuplicateIII.java
+++ 220-ContainsDuplicateIII/220-ContainsDuplicateIII.java
@@ -1,55 +1,50 @@
-// Last updated: 19/07/2026, 20:11:47
-1import java.util.HashMap;
-2import java.util.Map;
-3
-4class Solution {
-5    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
-6        if (nums == null || nums.length < 2 || indexDiff <= 0 || valueDiff < 0) {
-7            return false;
-8        }
+// Last updated: 19/07/2026, 20:12:34
+1import java.util.Stack;
+2
+3class Solution {
+4    public int calculate(String s) {
+5        Stack<Integer> stack = new Stack<>();
+6        int result = 0;
+7        int number = 0;
+8        int sign = 1; // 1 means positive, -1 means negative
 9
-10        // Map to store the bucket ID and the actual number currently in it
-11        Map<Long, Long> buckets = new HashMap<>();
-12        long width = (long) valueDiff + 1; // Size of each bucket
-13
-14        for (int i = 0; i < nums.length; i++) {
-15            long mappedVal = (long) nums[i];
-16            long bucketId = getBucketId(mappedVal, width);
-17
-18            // Condition 1: Current bucket already has a number
-19            if (buckets.containsKey(bucketId)) {
-20                return true;
-21            }
-22            
-23            // Condition 2: Check the adjacent left bucket
-24            if (buckets.containsKey(bucketId - 1) && Math.abs(mappedVal - buckets.get(bucketId - 1)) <= valueDiff) {
-25                return true;
-26            }
-27            
-28            // Condition 3: Check the adjacent right bucket
-29            if (buckets.containsKey(bucketId + 1) && Math.abs(mappedVal - buckets.get(bucketId + 1)) <= valueDiff) {
-30                return true;
-31            }
-32
-33            // Add the current number to its bucket
-34            buckets.put(bucketId, mappedVal);
-35
-36            // Maintain the sliding window size of indexDiff
-37            if (i >= indexDiff) {
-38                long oldBucketId = getBucketId((long) nums[i - indexDiff], width);
-39                buckets.remove(oldBucketId);
-40            }
-41        }
-42
-43        return false;
-44    }
-45
-46    // Helper to calculate bucket ID supporting negative numbers
-47    private long getBucketId(long val, long width) {
-48        if (val >= 0) {
-49            return val / width;
-50        } else {
-51            return (val + 1) / width - 1;
-52        }
-53    }
-54}
+10        for (int i = 0; i < s.length(); i++) {
+11            char c = s.charAt(i);
+12
+13            if (Character.isDigit(c)) {
+14                number = 10 * number + (c - '0');
+15            } else if (c == '+') {
+16                result += sign * number;
+17                number = 0;
+18                sign = 1;
+19            } else if (c == '-') {
+20                result += sign * number;
+21                number = 0;
+22                sign = -1;
+23            } else if (c == '(') {
+24                // Push the current result and sign onto the stack
+25                stack.push(result);
+26                stack.push(sign);
+27                
+28                // Reset the sign and result for the new sub-expression
+29                sign = 1;
+30                result = 0;
+31            } else if (c == ')') {
+32                result += sign * number;
+33                number = 0;
+34                
+35                // Multiply by the sign before the parenthesis
+36                result *= stack.pop();
+37                // Add to the result computed before the parenthesis
+38                result += stack.pop();
+39            }
+40        }
+41        
+42        // Add the last remaining number if any
+43        if (number != 0) {
+44            result += sign * number;
+45        }
+46        
+47        return result;
+48    }
+49}
