aa5c4c04d1aa24c0e0496dab90c8ed6f2e946b49
janani <jananijanani2611@gmail.com>
2026-07-23 22:46:51 +0530

    File updated at 23/07/2026, 22:46:51

--- 90-SubsetsII/90-SubsetsII.java
+++ 90-SubsetsII/90-SubsetsII.java
@@ -1,34 +1,38 @@
-// Last updated: 23/07/2026, 22:43:01
-1class Solution {
-2    private TreeNode first = null;
-3    private TreeNode second = null;
-4    private TreeNode prev = null;
+// Last updated: 23/07/2026, 22:46:51
+1import java.util.ArrayList;
+2import java.util.LinkedList;
+3import java.util.List;
+4import java.util.Queue;
 5
-6    public void recoverTree(TreeNode root) {
-7        inorder(root);
-8
-9       
-10        if (first != null && second != null) {
-11            int temp = first.val;
-12            first.val = second.val;
-13            second.val = temp;
-14        }
-15    }
-16
-17    private void inorder(TreeNode root) {
-18        if (root == null) return;
+6class Solution {
+7    public List<List<Integer>> levelOrder(TreeNode root) {
+8        List<List<Integer>> result = new ArrayList<>();
+9        if (root == null) {
+10            return result;
+11        }
+12
+13        Queue<TreeNode> queue = new LinkedList<>();
+14        queue.offer(root);
+15
+16        while (!queue.isEmpty()) {
+17            int levelSize = queue.size();
+18            List<Integer> currentLevel = new ArrayList<>();
 19
-20        inorder(root.left);
-21
-22       
-23        if (prev != null && root.val < prev.val) {
-24            if (first == null) {
-25                first = prev; 
-26            }
-27            second = root;    
-28        }
-29        prev = root;
-30
-31        inorder(root.right);
-32    }
-33}
+20            for (int i = 0; i < levelSize; i++) {
+21                TreeNode currentNode = queue.poll();
+22                currentLevel.add(currentNode.val);
+23
+24                if (currentNode.left != null) {
+25                    queue.offer(currentNode.left);
+26                }
+27                if (currentNode.right != null) {
+28                    queue.offer(currentNode.right);
+29                }
+30            }
+31
+32            result.add(currentLevel);
+33        }
+34
+35        return result;
+36    }
+37}
