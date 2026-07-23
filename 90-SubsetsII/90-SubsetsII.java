088409817bf65e02c0c5ef37a72777a2b6faea83
janani <jananijanani2611@gmail.com>
2026-07-23 21:50:09 +0530

    File updated at 23/07/2026, 21:50:05

--- 90-SubsetsII/90-SubsetsII.java
+++ 90-SubsetsII/90-SubsetsII.java
@@ -1,32 +1,28 @@
-// Last updated: 23/07/2026, 21:46:26
-1import java.util.ArrayList;
-2import java.util.Arrays;
-3import java.util.List;
-4
-5class Solution {
-6    public List<List<Integer>> subsetsWithDup(int[] nums) {
-7        List<List<Integer>> result = new ArrayList<>();
-8        
-9        Arrays.sort(nums);
-10        
-11        backtrack(0, nums, new ArrayList<>(), result);
-12        return result;
-13    }
-14
-15    private void backtrack(int start, int[] nums, List<Integer> current, List<List<Integer>> result) {
-16        result.add(new ArrayList<>(current));
-17
-18        for (int i = start; i < nums.length; i++) {
-19            
-20            if (i > start && nums[i] == nums[i - 1]) {
-21                continue;
+// Last updated: 23/07/2026, 21:50:05
+1class Solution {
+2    public int numDecodings(String s) {
+3        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+4            return 0; 
+5        }
+6
+7        int n = s.length();
+8        int[] dp = new int[n + 1];
+9
+10        dp[0] = 1; // Empty string has 1 valid decoding way
+11        dp[1] = 1; // Single valid non-zero character has 1 way
+12
+13        for (int i = 2; i <= n; i++) {
+14            int oneDigit = Integer.parseInt(s.substring(i - 1, i));
+15            if (oneDigit >= 1 && oneDigit <= 9) {
+16                dp[i] += dp[i - 1];
+17            }
+18
+19            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+20            if (twoDigits >= 10 && twoDigits <= 26) {
+21                dp[i] += dp[i - 2];
 22            }
-23
-24            current.add(nums[i]);
-25            
-26            backtrack(i + 1, nums, current, result);
-27            
-28            current.remove(current.size() - 1);
-29        }
-30    }
-31}
+23        }
+24
+25        return dp[n];
+26    }
+27}
