22f5d816406c9a345a7902f8d5a0d80e02ccc442
janani <jananijanani2611@gmail.com>
2026-07-30 08:53:31 +0530

    File updated at 30/07/2026, 08:53:30

--- 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.java
+++ 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,4 +1,4 @@
-// Last updated: 30/07/2026, 08:52:28
+// Last updated: 30/07/2026, 08:53:30
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -14,40 +14,40 @@
 13 *     }
 14 * }
 15 */
-16import java.util.*;
-17
-18class Solution {
-19    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-20        List<List<Integer>> result = new ArrayList<>();
-21        if (root == null) {
-22            return result;
-23        }
-24
-25        Queue<TreeNode> queue = new LinkedList<>();
-26        queue.add(root);
-27        boolean leftToRight = true;
-28
-29        while (!queue.isEmpty()) {
-30            int size = queue.size();
-31            LinkedList<Integer> currentLevel = new LinkedList<>();
-32
-33            for (int i = 0; i < size; i++) {
-34                TreeNode node = queue.poll();
-35
-36                if (leftToRight) {
-37                    currentLevel.addLast(node.val);
-38                } else {
-39                    currentLevel.addFirst(node.val);
-40                }
-41
-42                if (node.left != null) queue.add(node.left);
-43                if (node.right != null) queue.add(node.right);
-44            }
-45
-46            result.add(currentLevel);
-47            leftToRight = !leftToRight; 
-48        }
+16import java.util.HashMap;
+17import java.util.Map;
+18
+19class Solution {
+20    private int preorderIndex;
+21    private Map<Integer, Integer> inorderIndexMap;
+22
+23    public TreeNode buildTree(int[] preorder, int[] inorder) {
+24        preorderIndex = 0;
+25        inorderIndexMap = new HashMap<>();
+26
+27        // Store value -> index mappings for O(1) lookup in inorder array
+28        for (int i = 0; i < inorder.length; i++) {
+29            inorderIndexMap.put(inorder[i], i);
+30        }
+31
+32        return arrayToTree(preorder, 0, inorder.length - 1);
+33    }
+34
+35    private TreeNode arrayToTree(int[] preorder, int left, int right) {
+36    
+37        if (left > right) {
+38            return null;
+39        }
+40
+41        int rootVal = preorder[preorderIndex++];
+42        TreeNode root = new TreeNode(rootVal);
+43
+44    
+45        int rootInorderIndex = inorderIndexMap.get(rootVal);
+46
+47        root.left = arrayToTree(preorder, left, rootInorderIndex - 1);
+48        root.right = arrayToTree(preorder, rootInorderIndex + 1, right);
 49
-50        return result;
+50        return root;
 51    }
 52}
