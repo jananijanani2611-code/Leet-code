1c55ec934d381a2810da244ada4b57fbcc95ec9e
janani <jananijanani2611@gmail.com>
2026-07-30 08:55:47 +0530

    File updated at 30/07/2026, 08:55:47

--- 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.java
+++ 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,4 +1,4 @@
-// Last updated: 30/07/2026, 08:55:13
+// Last updated: 30/07/2026, 08:55:47
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -14,40 +14,33 @@
 13 *     }
 14 * }
 15 */
-16import java.util.HashMap;
-17import java.util.Map;
-18
-19class Solution {
-20    private int postorderIndex;
-21    private Map<Integer, Integer> inorderIndexMap;
-22
-23    public TreeNode buildTree(int[] inorder, int[] postorder) {
+16import java.util.*;
+17
+18class Solution {
+19    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+20        List<List<Integer>> result = new ArrayList<>();
+21        if (root == null) {
+22            return result;
+23        }
 24
-25        postorderIndex = postorder.length - 1;
-26        inorderIndexMap = new HashMap<>();
+25        Queue<TreeNode> queue = new LinkedList<>();
+26        queue.add(root);
 27
-28  
-29        for (int i = 0; i < inorder.length; i++) {
-30            inorderIndexMap.put(inorder[i], i);
-31        }
-32
-33        return arrayToTree(postorder, 0, inorder.length - 1);
-34    }
+28        while (!queue.isEmpty()) {
+29            int levelSize = queue.size();
+30            List<Integer> currentLevel = new ArrayList<>();
+31
+32            for (int i = 0; i < levelSize; i++) {
+33                TreeNode node = queue.poll();
+34                currentLevel.add(node.val);
 35
-36    private TreeNode arrayToTree(int[] postorder, int left, int right) {
-37
-38        if (left > right) {
-39            return null;
-40        }
-41
-42        int rootVal = postorder[postorderIndex--];
-43        TreeNode root = new TreeNode(rootVal);
-44
-45        int rootInorderIndex = inorderIndexMap.get(rootVal);
-46
-47        root.right = arrayToTree(postorder, rootInorderIndex + 1, right);
-48        root.left = arrayToTree(postorder, left, rootInorderIndex - 1);
-49
-50        return root;
-51    }
-52}
+36                if (node.left != null) queue.add(node.left);
+37                if (node.right != null) queue.add(node.right);
+38            }
+39
+40            result.add(0, currentLevel);
+41        }
+42
+43        return result;
+44    }
+45}
